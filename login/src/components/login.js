b94cfbeecb434cc6585d0be8704647dafb2fc742
Anargul Saitmuratova <anara.nabievna@mail.com>
2022-10-28 02:38:48 +0300

    Material UI Login page hw done

---
+++ login/src/components/login.js
@@ -0,0 +1,62 @@
+import { Avatar, Button, Grid, Paper, TextField, Typography, Link} from "@material-ui/core";
+import React from "react";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+const Login = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 280,
+    margin: "20px auto",
+  };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnStyle = { margin: "8px 0" };
+  return (
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2>Sign in</h2>
+        </Grid>
+        <TextField
+          label="Username"
+          placeholder="Enter username"
+          fullWidth
+          required
+        />
+        <TextField
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+        />
+
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={btnStyle}
+          fullWidth
+        >
+          Sign in
+        </Button>
+        <Typography>
+          <Link href="#">Forgot password?</Link>
+        </Typography>
+        <Typography>
+          Do you have an account?
+          <Link href="#">Sign Up</Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
+export default Login;
